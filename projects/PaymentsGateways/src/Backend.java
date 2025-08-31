bf0a1e31442a63f250a0ccba88d4d6e2374f1f9e
Pol Gutierrez <pol.gutierrez@students.salle.url.edu>
2025-08-31 18:23:06 +0200

    Basic setup

---
+++ projects/PaymentsGateways/src/Backend.java
@@ -0,0 +1,3 @@
+
+
+// in this class, there is going to be everything related with the backend of the page.
---
+++ projects/PaymentsGateways/src/FrontEnd.jsx
@@ -0,0 +1,3 @@
+
+
+// in this class, there is going to be everything related with the view of the page.
