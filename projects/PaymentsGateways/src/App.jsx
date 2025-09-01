06a7159180ef5237f60a685589b759d1340e22ec
Pol Gutierrez <pol.gutierrez@students.salle.url.edu>
2025-09-01 17:06:04 +0200

    adding the shopping cart button

--- projects/PaymentsGateways/src/App.jsx
+++ projects/PaymentsGateways/src/App.jsx
@@ -3,20 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import './App.css'
+import FrontEnd from './FrontEnd.jsx'
+// import Backend from './Backend.java'
 
 // main class that contains all the application logic.
 function App() {
   return (
-    <div className="container">
-      <h1>Payments Gateways</h1>
-      <p>This is a project to learn about payment gateways.</p>
-      <p>I'm going to use Stripe as payment gateway.</p>
-      <p>Also going to use React for the frontend and Java for the backend.</p>
-      <p>And finally Vite as the build tool.</p>
+    <div>
+      <div className="container">
+        <h1>Payments Gateways</h1>
+        <p>This is a project to learn about payment gateways.</p>
+        <p>I'm going to use Stripe as payment gateway.</p>
+        <p>Also going to use React for the frontend and Java for the backend.</p>
+        <p>And finally Vite as the build tool.</p>
+      </div>
+
+      <FrontEnd />
+      
     </div>
-
-    
-
   )
 }
 
--- projects/PaymentsGateways/src/FrontEnd.jsx
+++ projects/PaymentsGateways/src/FrontEnd.jsx
@@ -1,3 +1,16 @@
+import React from 'react'
 
-
+import CartButton from './CartButton.jsx'
 // in this class, there is going to be everything related with the view of the page.
+
+export default function FrontEnd() {
+  return (
+    <div>
+        <CartButton />
+    </div>
+  )
+}
+
+// en esta clase va a haber:
+// - boton de carrito de compra
+// - 3 componentes de los productos
