b250c2269e2842c1627ce377623fbf793dcbb775
Pol Gutierrez <pol.gutierrez@students.salle.url.edu>
2025-09-05 10:53:40 +0200

    adding another url for the payment process.

--- projects/PaymentsGateways/src/App.jsx
+++ projects/PaymentsGateways/src/App.jsx
@@ -6,19 +6,30 @@ import './App.css'
 import FrontEnd from './FrontEnd.jsx'
 // import Backend from './Backend.java'
 
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+
 // main class that contains all the application logic.
 function App() {
+  // variable that registers the actual url of the page:
+  const location = useLocation();
+
   return (
     <div className='app'>
-      <div className="container">
-        <h1>Payments Gateways</h1>
-        <p>This is a project to learn about payment gateways.</p>
-        <p>I'm going to use Stripe as payment gateway.</p>
-        <p>Also going to use React for the frontend and Java for the backend.</p>
-        <p>And finally Vite as the build tool.</p>
-      </div>
+      {location.pathname === '/' && (
+        <div className="container">
+          <h1>Payments Gateways</h1>
+          <p>This is a project to learn about payment gateways.</p>
+          <p>I'm going to use Stripe as payment gateway.</p>
+          <p>Also going to use React for the frontend and Java for the backend.</p>
+          <p>And finally Vite as the build tool.</p>
+          <p>If you want to feel the experience click on some products to be added in the cart for the payment.</p>
+        </div>
+      )}
 
-      <FrontEnd />
+        <Routes>
+          <Route path="/" element={<FrontEnd />} />          
+          <Route path="/cart" element={<h1>Cart Page - Under Construction</h1>} />
+        </Routes>
 
     </div>
   )
--- projects/PaymentsGateways/src/CartButton.jsx
+++ projects/PaymentsGateways/src/CartButton.jsx
@@ -1,10 +1,33 @@
 import './ShoppingCart.css'
 
+// I do import the library that allows me to change url:
+import { useNavigate } from 'react-router-dom'
+
+import { useState, useEffect } from 'react'
+
 // class that contains everything related with the cart button.
 export default function CartButton(props) {
+    const navigate = useNavigate();
+    const [buttonClicked, setButtonClicked] = useState(false);
+
+    // function that handles the click on the cart button.
+    function handleClick() {
+        // I change the state of the button to clicked.
+        setButtonClicked(true);
+    }
+
+    // use effect that navigates to the cart page when the button is clicked.
+    useEffect(() => {
+        if (buttonClicked) {
+            navigate('/cart');
+            // I reset the button clicked state to false.
+            setButtonClicked(false);
+        }
+    }, [buttonClicked]);
+
   return (
     <div className="cart-container">
-        <button className="cart-button cart-icon-button">
+        <button className="cart-button cart-icon-button" onClick={handleClick}>
             <img src="https://uxwing.com/wp-content/themes/uxwing/download/e-commerce-currency-shopping/shopping-cart-white-icon.png" alt="Carrito" className='cart-icon' /> 
             <span className="cart-count">{props.cartCount}</span>
         </button>
